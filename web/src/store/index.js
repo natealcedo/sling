8cc17d3a18848c316302162e9bfe589ef36fc298
Nathaniel D Alcedo Jr <ndaljr@gmail.com>
2018-07-07 15:16:06 +0800

    Hook up dev tools for redux

--- web/src/store/index.js
+++ web/src/store/index.js
@@ -1,11 +1,11 @@
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 import reducers from "../reducers";
 
-const middleWare = [thunk];
-
-const createStoreWithMiddleware = applyMiddleware(...middleWare)(createStore);
-
-const store = createStoreWithMiddleware(reducers);
+// eslint-disable-next-line
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducers, /* preloadedState, */ composeEnhancers(
+  applyMiddleware(thunk)
+));
 
 export default store;
