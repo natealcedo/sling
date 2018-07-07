cec253063a0fff86bba52dcf9f596b861fd1e80e
Nathaniel D Alcedo Jr <ndaljr@gmail.com>
2018-07-07 15:12:11 +0800

    Hook up all auth related matter with app

--- web/src/containers/App.js
+++ web/src/containers/App.js
@@ -1,34 +1,42 @@
 import React from "react";
 import { Router, Route, Switch } from "react-router-dom";
-import { func, shape } from "prop-types";
+import { bool, func, shape } from "prop-types";
 import { connect } from "react-redux";
-import { authenticate } from "../actions/session"
+import { authenticate, unauthenticate } from "../actions/session"
 import Home from "./Home";
 import Signup from './Signup';
 import Login from "./Login";
 import NotFound from "../components/NotFound";
+import MatchAuthenticated from "../components/MatchAuthenticated";
+import RedirectAuthenticated from "../components/RedirectAuthenticated";
 
 class App extends React.PureComponent {
   static propTypes = {
     history: shape({}).isRequired,
-    authenticate: func.isRequired
+    authenticate: func.isRequired,
+    unauthenticate: func.isRequired,
+    isAuthenticated: bool.isRequired,
+    willAuthenticate: bool.isRequired,
   }
 
   componentDidMount() {
     const token = localStorage.getItem('token');
     if (token) {
-      this.props.authenticate()(this.props.history);
+      return this.props.authenticate()(this.props.history);
     }
+    return this.props.unauthenticate()
   }
 
   render() {
+    const { isAuthenticated, willAuthenticate } = this.props;
+    const authProps = { isAuthenticated, willAuthenticate };
     return (
       <Router history={this.props.history}>
         <div style={{ display: 'flex', flex: 1 }}>
           <Switch>
-            <Route exact path="/" component={Home} />
-            <Route exact path="/signup" component={Signup} />
-            <Route exact path="/login" component={Login} />
+            <MatchAuthenticated exact path="/" component={Home} {...authProps} />
+            <RedirectAuthenticated exact path="/signup" component={Signup} {...authProps} />
+            <RedirectAuthenticated exact path="/login" component={Login} {...authProps} />
             <Route component={NotFound} />
           </Switch>
         </div>
@@ -38,4 +46,8 @@ class App extends React.PureComponent {
 }
 
 
-export default connect(null, { authenticate })(App);
+export default connect(state => ({
+  isAuthenticated: state.session.isAuthenticated,
+  currentUser: state.session.currentUser,
+  willAuthenticate: state.session.willAuthenticate
+}), { authenticate, unauthenticate })(App);
