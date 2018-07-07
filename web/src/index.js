484d6095c1a966a3d4f73d4e1a456bda5f69dfee
Nathaniel D Alcedo Jr <ndaljr@gmail.com>
2018-07-07 13:45:57 +0800

    Use history package and hook it up with application

--- web/src/index.js
+++ web/src/index.js
@@ -1,15 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
+import { createBrowserHistory } from "history"
 import store from "./store";
 import App from "./containers/App";
 import './styles/bootstrap.css';
 import './styles/font-awesome.css';
 import './styles/index.css';
 
+const history = createBrowserHistory();
+
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <App history={history} />
   </Provider>,
   document.getElementById("root")
 );
