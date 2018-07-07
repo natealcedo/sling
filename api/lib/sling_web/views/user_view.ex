94ae027fa7f269090c2ce5869877b24ed011402b
Nathaniel D Alcedo Jr <ndaljr@gmail.com>
2018-07-07 12:46:07 +0800

    Update user creation controller function to return a jwt in meta key object

--- api/lib/sling_web/views/user_view.ex
+++ api/lib/sling_web/views/user_view.ex
@@ -1,11 +1,14 @@
 defmodule SlingWeb.UserView do
   use SlingWeb, :view
 
-  def render("user.json", %{user: user}) do
+  def render("user.json", %{user: user, jwt: jwt}) do
     %{
       id: user.id,
       username: user.username,
-      email: user.email
+      email: user.email,
+      meta: %{
+        jwt: jwt
+      }
     }
   end
 end
