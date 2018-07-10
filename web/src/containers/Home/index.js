bfa9e523a2849b3a9eb6732ec8e0c871865dc8e2
Nathaniel D Alcedo Jr <ndaljr@gmail.com>
2018-07-10 22:24:59 +0800

    Use new components in Home component

--- web/src/containers/Home/index.js
+++ web/src/containers/Home/index.js
@@ -1,5 +1,75 @@
-import React from "react";
+import React, { Component } from 'react';
+import propTypes from "prop-types";
+import { connect } from 'react-redux';
+import { css, StyleSheet } from 'aphrodite';
+import { fetchRooms, createRoom, joinRoom } from '../../actions/room';
+import NewRoomForm from '../../components/NewRoomForm';
+import Navbar from '../../components/Navbar';
+import RoomListItem from '../../components/RoomListItem';
 
-const Home = () => <div>Home</div>;
+const styles = StyleSheet.create({
+  card: {
+    maxWidth: '500px',
+    padding: '3rem 4rem',
+    margin: '2rem auto',
+  },
+});
 
-export default Home;
+
+class Home extends Component {
+
+  static propTypes = {
+    history: propTypes.func.isRequired,
+    rooms: propTypes.array.isRequired,
+    currentUserRooms: propTypes.array.isRequired,
+    fetchRooms: propTypes.func.isRequired,
+    createRoom: propTypes.func.isRequired,
+    joinRoom: propTypes.func.isRequired,
+  }
+
+  componentDidMount() {
+    this.props.fetchRooms();
+  }
+
+  handleNewRoomSubmit = data => this.props.createRoom(data, this.props.history);
+
+  handleRoomJoin = roomId => this.props.joinRoom(roomId, this.props.history);
+
+  renderRooms() {
+    const currentUserRoomIds = [];
+    this.props.currentUserRooms.map(room => currentUserRoomIds.push(room.id));
+
+    return this.props.rooms.map(room =>
+      <RoomListItem
+        key={room.id}
+        room={room}
+        onRoomJoin={this.handleRoomJoin}
+        currentUserRoomIds={currentUserRoomIds}
+      />
+    );
+  }
+
+  render() {
+    return (
+      <div style={{ flex: '1' }}>
+        <Navbar />
+        <div className={`card ${css(styles.card)}`}>
+          <h3 style={{ marginBottom: '2rem', textAlign: 'center' }}>Create a new room</h3>
+          <NewRoomForm onSubmit={this.handleNewRoomSubmit} />
+        </div>
+        <div className={`card ${css(styles.card)}`}>
+          <h3 style={{ marginBottom: '2rem', textAlign: 'center' }}>Join a room</h3>
+          {this.renderRooms()}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default connect(
+  state => ({
+    rooms: state.rooms.all,
+    currentUserRooms: state.rooms.currentUserRooms,
+  }),
+  { fetchRooms, createRoom, joinRoom }
+)(Home);
