765ec5de0a75690dcff8fef95bf8180ae936bbfb
Nathaniel D Alcedo Jr <ndaljr@gmail.com>
2018-07-10 21:40:15 +0800

    Implement actions and reducers

---
+++ web/src/actions/room.js
@@ -0,0 +1,31 @@
+import api from "../api";
+
+export function fetchRooms() {
+  return dispatch => api.fetch('/rooms')
+    .then((response) => {
+      dispatch({ type: 'FETCH_ROOMS_SUCCESS', response });
+    });
+}
+
+export function fetchUserRooms(userId) {
+  return dispatch => api.fetch(`/users/${userId}/rooms`)
+    .then((response) => {
+      dispatch({ type: 'FETCH_USER_ROOMS_SUCCESS', response });
+    });
+}
+
+export function createRoom(data, history) {
+  return dispatch => api.post('/rooms', data)
+    .then((response) => {
+      dispatch({ type: 'CREATE_ROOM_SUCCESS', response });
+      history.push(`/r/${response.data.id}`);
+    });
+}
+
+export function joinRoom(roomId, history) {
+  return dispatch => api.post(`/rooms/${roomId}/join`)
+    .then((response) => {
+      dispatch({ type: 'ROOM_JOINED', response });
+      history.push(`/r/${response.data.id}`);
+    });
+}
