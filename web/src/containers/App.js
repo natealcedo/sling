4870c1635d933de4427dc9a80dac0271d64afffb
Nathaniel D Alcedo Jr <ndaljr@gmail.com>
2018-07-10 22:13:42 +0800

    Add in sidebar, update authenticate function call

--- web/src/containers/App.js
+++ web/src/containers/App.js
@@ -1,14 +1,15 @@
 import React from "react";
 import { Router, Route, Switch } from "react-router-dom";
-import { bool, func, shape } from "prop-types";
+import { array, bool, func, shape } from "prop-types";
 import { connect } from "react-redux";
-import { authenticate, unauthenticate } from "../actions/session"
+import { authenticate, unauthenticate, logout } from "../actions/session"
 import Home from "./Home";
 import Signup from './Signup';
 import Login from "./Login";
 import NotFound from "../components/NotFound";
 import MatchAuthenticated from "../components/MatchAuthenticated";
 import RedirectAuthenticated from "../components/RedirectAuthenticated";
+import SideBar from "../components/Sidebar";
 
 class App extends React.PureComponent {
   static propTypes = {
@@ -17,22 +18,34 @@ class App extends React.PureComponent {
     unauthenticate: func.isRequired,
     isAuthenticated: bool.isRequired,
     willAuthenticate: bool.isRequired,
+    currentUserRooms: array.isRequired,
+    logout: func.isRequired,
   }
 
   componentDidMount() {
     const token = localStorage.getItem('token');
     if (token) {
-      return this.props.authenticate()(this.props.history);
+      return this.props.authenticate(this.props.history);
     }
     return this.props.unauthenticate()
   }
 
+  handleLogout = history => this.props.logout(history);
+
   render() {
-    const { isAuthenticated, willAuthenticate } = this.props;
+    const { isAuthenticated, willAuthenticate, history, currentUserRooms } = this.props;
     const authProps = { isAuthenticated, willAuthenticate };
     return (
       <Router history={this.props.history}>
         <div style={{ display: 'flex', flex: 1 }}>
+          {
+            isAuthenticated &&
+            <SideBar
+              history={history}
+              rooms={currentUserRooms}
+              onLogoutClick={this.handleLogout}
+            />
+          }
           <Switch>
             <MatchAuthenticated exact path="/" component={Home} {...authProps} />
             <RedirectAuthenticated exact path="/signup" component={Signup} {...authProps} />
@@ -49,5 +62,6 @@ class App extends React.PureComponent {
 export default connect(state => ({
   isAuthenticated: state.session.isAuthenticated,
   currentUser: state.session.currentUser,
-  willAuthenticate: state.session.willAuthenticate
-}), { authenticate, unauthenticate })(App);
+  willAuthenticate: state.session.willAuthenticate,
+  currentUserRooms: state.rooms.currentUserRooms,
+}), { authenticate, unauthenticate, logout })(App);
